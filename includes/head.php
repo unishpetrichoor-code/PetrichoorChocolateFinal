d534e0c8c241dd33210d319fa8b9d9a7f01398d9
Unish Bhattarai <unish.petrichoor@gmail.com>
2026-02-07 17:23:41 +0400

    small changes

--- includes/head.php
+++ includes/head.php
@@ -25,18 +25,4 @@ $cssFile = ($_SESSION['lang'] === 'ar') ? 'style_ar.css' : 'style.css';
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Petrichoor Chocolate</title>
 
-  <!-- Bootstrap 5 CDN -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-
-
-  <!-- Custom CSS -->
-  <link rel="stylesheet" href="/petrichoor/assets/css/style.css">
-
-  <!-- Google Fonts for Marquee -->
-<link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&display=swap" rel="stylesheet">
-
-<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" rel="stylesheet">
-
 </head>
